a8570f565faa25fd949dc18829c06a217e81441a
Francisco Taveira <ftaveira.dev@gmail.com>
2022-11-17 00:18:44 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import NotificationButton from './components/NotificationButton'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return ( <h1>Olá Mundo</h1>
+  return (
+    <>
+      <h1>Olá Mundo</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
+
   )
 }
 
